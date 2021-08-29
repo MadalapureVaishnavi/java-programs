20d0a2808663e4a0c391d0c8fa5ec63a68c23bbb
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-29 16:07:06 +0530

    Add files via upload

---
+++ ind16.java
@@ -0,0 +1,26 @@
+public class Ind16{
+public static void main(String args[])
+{
+System.out.println("Check whether the primitive values crossing the limits or not ::");
+System.out.println("------------------------------------------------------------");
+char gender = 'F';             
+boolean isEmployed =true;         
+short numOfsons=2;
+short yearOfAppt = 2009;      
+int YearlyPackage = 1500000;  
+double height = 79.48;     
+float gpa = 4.69f;           
+long totalDrawan = 12047235L;     
+long balance = 995324987L;
+
+System.out.println("The Gender is : " +gender);
+System.out.println("Is she married? : " +isEmployed);
+System.out.println("Number of sons she has :"+ numOfsons);
+System.out.println("Year of her appointment : "+ yearOfAppt);
+System.out.println("Salary for a year :  " +YearlyPackage);
+System.out.println("Height is :  " +height);
+System.out.println("GPA is : " +gpa);
+System.out.println("Salary drawn upto :  " +totalDrawan);
+System.out.println("Balance till: " +balance);
+}
+}
