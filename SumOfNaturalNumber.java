3328980c0773a5cc05aeec8d0b79adc781a96ba6
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-29 12:20:51 +0530

    Add files via upload

---
+++ SumOfNaturalNumber.java
@@ -0,0 +1,17 @@
+//Write a program to calculate the sum of first 10 natural number
+
+public class SumOfNaturalNumber
+{  
+public static void main(String[] args)   
+{  
+int i, num = 10, sum = 0;  
+//executes until the condition returns true  
+for(i = 1; i <= num; ++i)  
+{  
+//adding the value of i into sum variable  
+sum = sum + i;  
+}  
+//prints the sum   
+System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+}  
+}  
