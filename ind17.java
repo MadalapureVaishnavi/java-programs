81f62106589a131bd51a25f9f006d4e9f3cbcaaa
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-29 16:08:33 +0530

    Add files via upload

---
+++ ind17.java
@@ -0,0 +1,24 @@
+public class Ind16{
+public static void main(String args[])
+{
+System.out.println("Display arithmetic operations with mixed data type:");
+System.out.println("-------------------------------------------------------");
+
+int a=5 ,b=7;
+double c=3.7,d=8.0;
+
+System.out.println("5+7= " + (a+b));
+System.out.println("3.7+8.0= "+(c+d));
+System.out.println("5+8.0= "+(a+d));
+System.out.println("5-7= "+(a-b));
+System.out.println("3.7-8.0="+(c-d));
+System.out.println("5-8.0="+(a-d));
+System.out.println("5*7="+(a*b));
+System.out.println("3.7*8.0="+(c*d));
+System.out.println("5*8.0="+(a*d));
+System.out.println("5/7="+(a/b));
+System.out.println("3.7/8.0="+(c/d));
+System.out.println("5/8.0="+(a/d));
+
+}
+}
