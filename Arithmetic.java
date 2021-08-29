d98382849759206ab03093336b86646ccbd83bb7
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-29 11:32:50 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+//Java Program to Add, subtract, multiply and divide Two Numbers
+
+public class Arithmetic {
+	public static void main(String[] args) {
+		int a=20,b=20;
+		System.out.println("addition:"+(a+b));
+		
+			System.out.println("subtraction:"+(a-b));
+			
+				System.out.println("multiplication:"+(a*b));
+				
+					System.out.println("division:"+(a/b));
+	}
+}
