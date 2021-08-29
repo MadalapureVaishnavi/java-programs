d3bd2fed1b2d685b9492d60d3bb94a3f6b0b423f
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-29 12:02:06 +0530

    Add files via upload

---
+++ LengthStr .java
@@ -0,0 +1,8 @@
+//Java Program to calculate length of  Strings
+
+public class LengthStr {
+	public static void main(String[] args) {
+		String a="java";
+		System.out.println(" length of string:" + a.length ());
+	}
+}
