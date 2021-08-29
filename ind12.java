7bd827df69a32ad6db4006d2edab45e307f7c0f6
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-29 15:58:30 +0530

    Add files via upload

---
+++ ind12.java
@@ -0,0 +1,10 @@
+public class Add
+{
+	public static void main(String[] args)
+	 {
+		System.out.println("Print the sum of two numbers :");
+		
+			System.out.println("-------------------------------");
+			System.out.println("The sum of 29 and 30 is :" +(29+30));
+	}
+}
