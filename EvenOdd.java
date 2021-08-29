7febcfe7a9b0b514f0dcc61271998f076adf6ff1
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-29 11:30:29 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+//Java Program to Check Whether Number is Even or Odd
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int n=13;
+		if(n % 2==0)
+		System. out.println("even number is :" +n);
+		else
+			System. out.println("odd number is :" +n);
+		
+	}
+}
